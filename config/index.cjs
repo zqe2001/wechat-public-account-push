8fd8039bfa8a03e81be30d2e10cf2395fb478431
zhuquanen <87791866+zqe2001@users.noreply.github.com>
2023-02-08 16:59:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'pBCe_HtxkBLm9UnuNc4kuWnKYiSE9wOEoxki92m-R-Q',
+  CALLBACK_TEMPLATE_ID: '	lA_qe8Ezn7qp3gyVuwD5r4pnTGk4bsFK1DZTx930apE',
 
   CALLBACK_USERS: [
     {
